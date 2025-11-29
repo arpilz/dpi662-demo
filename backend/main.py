157ab5d2645bfee84f5d009874f317c53e89c5be
arpilz <175361225+arpilz@users.noreply.github.com>
2025-11-29 16:46:13 -0500

    basic structure

---
+++ backend/main.py
@@ -0,0 +1,4 @@
+# mock api (usds seemed to use postgresql, so we can simulate that?)
+# might not build out anything here until the very end, since
+# we're focusing on front-end design primarily. but i wanted the
+# repo set up so that the architecture is all there.
