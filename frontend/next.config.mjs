b1810503fd7c98414cb4967f39201657d5526b4f
arpilz <175361225+arpilz@users.noreply.github.com>
2025-11-29 21:36:11 -0500

    preparing to deploy landing page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,20 +1,7 @@
-// next.config.mjs
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output static site
-  output: "export",
-
-  // for gh pages
-  images: {
-    unoptimized: true,
-  },
-
-  // If this is a project page (username.github.io/my-repo)
-  // set basePath + assetPrefix to "/my-repo"
-  basePath: isProd ? "/dpi662-demo" : "",
-  assetPrefix: isProd ? "/dpi662-demo/" : "",
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
