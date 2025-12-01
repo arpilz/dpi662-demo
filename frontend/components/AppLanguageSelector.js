5e897b94e6db5deecb4e2345f23ce9013c657727
arpilz <175361225+arpilz@users.noreply.github.com>
2025-11-30 22:37:18 -0500

    languages

--- frontend/components/AppLanguageSelector.js
+++ frontend/components/AppLanguageSelector.js
@@ -28,6 +28,18 @@ export default function AppLanguageSelector() {
         <option value="de">Deutsch (German)</option>
         <option value="ar">العربية (Arabic)</option>
         <option value="hi">हिन्दी (Hindi)</option>
+        <option value="bn">বাংলা (Bengali)</option>
+        <option value="he">עברית (Hebrew)</option>
+        <option value="id">bahasa Indonesia (Indonesian)</option>
+        <option value="it">Italiano (Italian)</option>
+        <option value="ja">日本語 (Japanese)</option>
+        <option value="ko">한국어 (Korean)</option>
+        <option value="fa">فارسی (Farsi)</option>
+        <option value="th">าษาไทย (Thai)</option>
+        <option value="ha">هَرْشٜن هَوْس (Hausa)</option>
+        <option value="pa">ਪੰਜਾਬੀ (Punjabi)</option>
+        <option value="pl">Polski (Polish)</option>
+        <option value="ru">Русский язык (Russian)</option>
       </select>
     </div>
   );
